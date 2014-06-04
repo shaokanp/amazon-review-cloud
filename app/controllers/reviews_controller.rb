dc9708d1073ff173bbb0081eac73839c2d2f2cfa
shaokanp <whobirdsyou@gmail.com>
2014-06-04 23:00:56 +0800

    Review#index paging

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,7 @@ class ReviewsController < ApplicationController
 	# params: product_id, keyword
 	def index
 		product = Product.where(productId: params[:product_id]).first
-    @reviews = product.reviews.where(text: /.*#{params[:keyword]}*/i)
+    @reviews = product.reviews.where(text: /.*#{params[:keyword]}*/i).limit(10).skip(params[:since].to_i)
     respond_to do |format|
       format.json {
         render json: @reviews
